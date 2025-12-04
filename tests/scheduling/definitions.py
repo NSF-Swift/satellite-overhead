38ee9549848b9c58a43baa493eaf1e99ffce8ea5
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2025-12-04 15:04:12 -0700

    refactor: Remove unused code.

--- tests/scheduling/definitions.py
+++
@@ -1,9 +0,0 @@
-from sopp.models.coordinates import Coordinates
-from sopp.models.facility import Facility
-
-ARBITRARY_FACILITY = Facility(
-    beamwidth=3.0,
-    coordinates=Coordinates(latitude=1.0, longitude=2.0),
-    elevation=1.0,
-    name="name",
-)
